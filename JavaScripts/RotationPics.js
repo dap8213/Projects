76010df30596d0addcf71aeda7f2cb75714afe21
dap8213 <dap8213@yahoo.com>
2017-10-11 17:24:53 -0700

    Add files via upload

---
+++ JavaScripts/RotationPics.js
@@ -0,0 +1,18 @@
+function start() {
+    var rotator = document.getElementById('images');
+    var delayInSeconds = 2;
+
+    var images = ['Beach.jpg', 'Colombia.jpg', 'Island.jpg', 'castle.jpg', 'China.jpg'];
+    var num = 0;
+    var changeImage = function() {
+        var len = images.length;
+        rotator.src = images[num++];
+        if (num == len) {
+            num = 0;
+        }
+    };
+    setInterval(changeImage, delayInSeconds * 1000);
+};
+window.onload=function(){
+start();
+}
